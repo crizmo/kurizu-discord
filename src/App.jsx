18607ac95078d83af2763affd93dec66bb93e665
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-06-25 21:10:40 +0530

    🚧 Rollback

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
 import { Box } from "@mui/material";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// info
-import Home from "./pages/info/home";
-import Announcements from "./pages/info/announcements";
-import About from "./pages/info/about";
-
-// general
-import General from "./pages/general/general";
-
-// projects
-import AnyAnime from "./pages/projects/anyanime";
-import Image2Url from "./pages/projects/image2url";
-import DiscordCards from "./pages/projects/discordcards";
-import Elina from "./pages/projects/elina";
-import Cordle from "./pages/projects/cordle";
-import Timely from "./pages/projects/timely";
-import Emogit from "./pages/projects/emogit";
-import Minikey from "./pages/projects/minikey";
-import Type3d from "./pages/projects/type3d";
-import Pixit from "./pages/projects/pixit";
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import SidebarWidget from "./widgets/sidebar/sidebarWidget"
 
 import ChannelNavbar from "./widgets/navbar/channelNav";
@@ -29,7 +7,7 @@ import Navbar from "./widgets/navbar/navbar";
 
 import ChannelBar from "./widgets/channelBar/channelsBar";
 
-// import Content from "./components/Routes/content.jsx";
+import Content from "./components/Routes/content.jsx";
 import MessageBar from "./widgets/messagebar/messagebar";
 
 import UserBar from "./widgets/userbar/userbar";
@@ -88,45 +66,23 @@ function App() {
   }
 
   return (
-    // <Router>
-    <div>
-      <Router>
-        {!isMobile ? (
-          <Box width="100vw" display="flex" className="App" overflow="hidden" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
-            <Box flexBasis="4.5rem" className="sidebar">
-              <SidebarWidget />
-            </Box>
-            <Box flexBasis="99vw">
-              <Box display="flex" >
-                <Box flexBasis={!isMobile ? "15rem" : null} >
-                  <ChannelNavbar />
-                  <ChannelBar setNavTitle={setNavTitle} />
-                </Box>
+    <Router>
+      {!isMobile ? (
+        <Box width="100vw" display="flex" className="App" overflow="hidden" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+          <Box flexBasis="4.5rem" className="sidebar">
+            <SidebarWidget />
+          </Box>
+          <Box flexBasis="99vw">
+            <Box display="flex" >
+              <Box flexBasis={!isMobile ? "15rem" : null} >
+                <ChannelNavbar />
+                <ChannelBar setNavTitle={setNavTitle} />
+              </Box>
                 <Box flexBasis="90vw" overflow="auto" bgcolor="#2f3136">
                   <Navbar navTitle={navTitle} />
                   <Box display="flex">
                     <Box flexBasis="75vw">
-                      <Routes>
-                        {/* info */}
-                        <Route path="/" element={<Home />} />
-                        <Route path="/announcements" element={<Announcements />} />
-                        <Route path="/about" element={<About />} />
-
-                        {/* general */}
-                        <Route path="/general" element={<General />} />
-
-                        {/* projects */}
-                        <Route path="/anyanime" element={<AnyAnime />} />
-                        <Route path="/image2url" element={<Image2Url />} />
-                        <Route path="/discordcards" element={<DiscordCards />} />
-                        <Route path="/elina" element={<Elina />} />
-                        <Route path="/cordle" element={<Cordle />} />
-                        <Route path="/timely" element={<Timely />} />
-                        <Route path="/emogit" element={<Emogit />} />
-                        <Route path="/minikey" element={<Minikey />} />
-                        <Route path="/type3d" element={<Type3d />} />
-                        <Route path="/pixit" element={<Pixit />} />
-                      </Routes>
+                      <Content />
                       <MessageBar />
                     </Box>
                     <Box flexBasis="15rem">
@@ -134,98 +90,76 @@ function App() {
                     </Box>
                   </Box>
                 </Box>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+        <Box width="100vw" display="flex" className="App" overflow="hidden" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+          <Box flexBasis="100vw" bgcolor="#2f3136" overflow={isMobile ? "hidden" : "auto"}>
+            <Navbar navTitle={navTitle} />
+            <Box display="flex">
+              <Box flexBasis="100vw">
+                <Content />
+                <MessageBar />
               </Box>
             </Box>
           </Box>
-        ) : (
-          <Box width="100vw" display="flex" className="App" overflow="hidden" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+          <SlidingPane
+            isOpen={state.isPaneOpenLeft}
+            from="left"
+            width="80vw"
+            onRequestClose={() => setState({ isPaneOpenLeft: false })}
+            className="sliding-pane-left"
+            hideHeader={true}
+          >
             <Box flexBasis="100vw" bgcolor="#2f3136" overflow={isMobile ? "hidden" : "auto"}>
-              <Navbar navTitle={navTitle} />
-              <Box display="flex">
-                <Box flexBasis="100vw">
-                  <Routes>
-                    {/* info */}
-                    <Route path="/" element={<Home />} />
-                    <Route path="/announcements" element={<Announcements />} />
-                    <Route path="/about" element={<About />} />
-
-                    {/* general */}
-                    <Route path="/general" element={<General />} />
-
-                    {/* projects */}
-                    <Route path="/anyanime" element={<AnyAnime />} />
-                    <Route path="/image2url" element={<Image2Url />} />
-                    <Route path="/discordcards" element={<DiscordCards />} />
-                    <Route path="/elina" element={<Elina />} />
-                    <Route path="/cordle" element={<Cordle />} />
-                    <Route path="/timely" element={<Timely />} />
-                    <Route path="/emogit" element={<Emogit />} />
-                    <Route path="/minikey" element={<Minikey />} />
-                    <Route path="/type3d" element={<Type3d />} />
-                    <Route path="/pixit" element={<Pixit />} />
-                  </Routes>
-                  <MessageBar />
+              <Box display="flex" flexDirection="column" height="100vh" sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              }}>
+                <Box display="flex" >
+                  <Box flexBasis="5rem">
+                    <SidebarWidget />
+                  </Box>
+                  <Box flexBasis="95.5vw">
+                    <Box flexBasis={null} >
+                      <ChannelNavbar />
+                      <ChannelBar setNavTitle={setNavTitle} />
+                    </Box>
+                  </Box>
                 </Box>
               </Box>
             </Box>
-            <SlidingPane
-              isOpen={state.isPaneOpenLeft}
-              from="left"
-              width="80vw"
-              onRequestClose={() => setState({ isPaneOpenLeft: false })}
-              className="sliding-pane-left"
-              hideHeader={true}
-            >
-              <Box flexBasis="100vw" bgcolor="#2f3136" overflow={isMobile ? "hidden" : "auto"}>
-                <Box display="flex" flexDirection="column" height="100vh" sx={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                }}>
-                  <Box display="flex" >
-                    <Box flexBasis="5rem">
-                      <SidebarWidget />
-                    </Box>
-                    <Box flexBasis="95.5vw">
-                      <Box flexBasis={null} >
-                        <ChannelNavbar />
-                        <ChannelBar setNavTitle={setNavTitle} />
-                      </Box>
-                    </Box>
-                  </Box>
+          </SlidingPane>
+          <SlidingPane
+            isOpen={state.isPaneOpenRight}
+            from="right"
+            width="70vw"
+            onRequestClose={() => setState({ isPaneOpenRight: false })}
+            className="sliding-pane-right"
+            hideHeader={true}
+          >
+            <Box flexBasis="100vw" bgcolor="#fff" onClick={() => setState({ isPaneOpenRight: false })}
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              }}>
+              <Box display="flex" >
+                <Box flexBasis="70vw">
+                  <UserBar />
                 </Box>
               </Box>
-            </SlidingPane>
-            <SlidingPane
-              isOpen={state.isPaneOpenRight}
-              from="right"
-              width="70vw"
-              onRequestClose={() => setState({ isPaneOpenRight: false })}
-              className="sliding-pane-right"
-              hideHeader={true}
-            >
-              <Box flexBasis="100vw" bgcolor="#fff" onClick={() => setState({ isPaneOpenRight: false })}
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                }}>
-                <Box display="flex" >
-                  <Box flexBasis="70vw">
-                    <UserBar />
-                  </Box>
-                </Box>
-              </Box>
-            </SlidingPane>
-          </Box>
-        )}
-      </Router>
-      {/* </Router> */}
-    </div>
+            </Box>
+          </SlidingPane>
+        </Box>
+      )}
+    </Router>
   );
 }
 
--- src/components/Routes/content.jsx
+++ src/components/Routes/content.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
 // info
 import Home from "../../pages/info/home";
-import Announcements from "../../pages/info/announcements";
+import Anouncements from "../../pages/info/announcements";
 import About from "../../pages/info/about";
 
 // general
@@ -21,29 +21,29 @@ import Type3d from "../../pages/projects/type3d";
 import Pixit from "../../pages/projects/pixit";
 
 const Content = ({ data }) => {
-  return (
-      <Routes>
-        {/* info */}
-        <Route path="/" element={<Home />} />
-        <Route path="/announcements" element={<Announcements />} />
-        <Route path="/about" element={<About />} />
+    return (
+        <Routes>
+            {/* info */}
+            <Route path="/" element={<Home />} />
+            <Route path="/announcements" element={<Anouncements />} />
+            <Route path="/about" element={<About />} />
 
-        {/* general */}
-        <Route path="/general" element={<General data={data} />} />
+            {/* general */}
+            <Route path="/general" element={<General data={data} />} />
 
-        {/* projects */}
-        <Route path="/anyanime" element={<AnyAnime />} />
-        <Route path="/image2url" element={<Image2Url />} />
-        <Route path="/discordcards" element={<DiscordCards />} />
-        <Route path="/elina" element={<Elina />} />
-        <Route path="/cordle" element={<Cordle />} />
-        <Route path="/timely" element={<Timely />} />
-        <Route path="/emogit" element={<Emogit />} />
-        <Route path="/minikey" element={<Minikey />} />
-        <Route path="/type3d" element={<Type3d />} />
-        <Route path="/pixit" element={<Pixit />} />
-      </Routes>
-  );
-};
+            {/* projects */}
+            <Route path="/anyanime" element={<AnyAnime />} />
+            <Route path="/image2url" element={<Image2Url />} />
+            <Route path="/discordcards" element={<DiscordCards />} />
+            <Route path="/elina" element={<Elina />} />
+            <Route path="/cordle" element={<Cordle />} />
+            <Route path="/timely" element={<Timely />} />
+            <Route path="/emogit" element={<Emogit />} />
+            <Route path="/minikey" element={<Minikey />} />
+            <Route path="/type3d" element={<Type3d />} />
+            <Route path="/pixit" element={<Pixit />} />
+        </Routes>
+    );
+}
 
 export default Content;
