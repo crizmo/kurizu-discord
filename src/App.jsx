f6d29df8b38c499cecdb23cae302916ad0861132
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-06-25 22:46:56 +0530

    🚧 HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import SidebarWidget from "./widgets/sidebar/sidebarWidget"
 
 import ChannelNavbar from "./widgets/navbar/channelNav";
@@ -12,9 +13,28 @@ import MessageBar from "./widgets/messagebar/messagebar";
 
 import UserBar from "./widgets/userbar/userbar";
 
-
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
+
+// info
+import Home from "./pages/info/home";
+import Anouncements from "./pages/info/announcements";
+import About from "./pages/info/about";
+
+// general
+import General from "./pages/general/general";
+
+// projects
+import AnyAnime from "./pages/projects/anyanime";
+import Image2Url from "./pages/projects/image2url";
+import DiscordCards from "./pages/projects/discordcards";
+import Elina from "./pages/projects/elina";
+import Cordle from "./pages/projects/cordle";
+import Timely from "./pages/projects/timely";
+import Emogit from "./pages/projects/emogit";
+import Minikey from "./pages/projects/minikey";
+import Type3d from "./pages/projects/type3d";
+import Pixit from "./pages/projects/pixit";
 
 import { useState } from "react";
 
@@ -78,18 +98,38 @@ function App() {
                 <ChannelNavbar />
                 <ChannelBar setNavTitle={setNavTitle} />
               </Box>
-                <Box flexBasis="90vw" overflow="auto" bgcolor="#2f3136">
-                  <Navbar navTitle={navTitle} />
-                  <Box display="flex">
-                    <Box flexBasis="75vw">
-                      <Content />
-                      <MessageBar />
-                    </Box>
-                    <Box flexBasis="15rem">
-                      <UserBar />
-                    </Box>
+              <Box flexBasis="90vw" overflow="auto" bgcolor="#2f3136">
+                <Navbar navTitle={navTitle} />
+                <Box display="flex">
+                  <Box flexBasis="75vw">
+                    <Routes>
+                      {/* info */}
+                      <Route path="/" element={<Home />} />
+                      <Route path="/announcements" element={<Anouncements />} />
+                      <Route path="/about" element={<About />} />
+
+                      {/* general */}
+                      <Route path="/general" element={<General />} />
+
+                      {/* projects */}
+                      <Route path="/anyanime" element={<AnyAnime />} />
+                      <Route path="/image2url" element={<Image2Url />} />
+                      <Route path="/discordcards" element={<DiscordCards />} />
+                      <Route path="/elina" element={<Elina />} />
+                      <Route path="/cordle" element={<Cordle />} />
+                      <Route path="/timely" element={<Timely />} />
+                      <Route path="/emogit" element={<Emogit />} />
+                      <Route path="/minikey" element={<Minikey />} />
+                      <Route path="/type3d" element={<Type3d />} />
+                      <Route path="/pixit" element={<Pixit />} />
+                    </Routes>
+                    <MessageBar />
+                  </Box>
+                  <Box flexBasis="15rem">
+                    <UserBar />
                   </Box>
                 </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
@@ -99,7 +139,27 @@ function App() {
             <Navbar navTitle={navTitle} />
             <Box display="flex">
               <Box flexBasis="100vw">
-                <Content />
+                <Routes>
+                  {/* info */}
+                  <Route path="/" element={<Home />} />
+                  <Route path="/announcements" element={<Anouncements />} />
+                  <Route path="/about" element={<About />} />
+
+                  {/* general */}
+                  <Route path="/general" element={<General />} />
+
+                  {/* projects */}
+                  <Route path="/anyanime" element={<AnyAnime />} />
+                  <Route path="/image2url" element={<Image2Url />} />
+                  <Route path="/discordcards" element={<DiscordCards />} />
+                  <Route path="/elina" element={<Elina />} />
+                  <Route path="/cordle" element={<Cordle />} />
+                  <Route path="/timely" element={<Timely />} />
+                  <Route path="/emogit" element={<Emogit />} />
+                  <Route path="/minikey" element={<Minikey />} />
+                  <Route path="/type3d" element={<Type3d />} />
+                  <Route path="/pixit" element={<Pixit />} />
+                </Routes>
                 <MessageBar />
               </Box>
             </Box>
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 // // vite.config.js
 import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Other Vite configuration options...
-  plugins : [react()],
-  server: {
-    // Ensure that the server fallback is set to index.html
-    // This allows React Router to handle the routing on the client-side
-    fs: {
-      strict: false,
-    },
-    historyApiFallback: true,
-  },
-});
+  plugins: [react()],
+})
