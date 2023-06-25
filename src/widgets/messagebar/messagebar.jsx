2bc6c50aa76e421c210cb0b3f0a00be9688d47a5
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-06-25 17:01:41 +0530

    🔮 UTC

--- src/widgets/messagebar/messagebar.jsx
+++ src/widgets/messagebar/messagebar.jsx
@@ -12,18 +12,9 @@ const MessageBar = () => {
     
     const sendMessage = async () => {
         try {
-            const currentDate = new Date();
-            const year = currentDate.getFullYear();
-            const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-            const day = String(currentDate.getDate()).padStart(2, '0');
-            const hours = String(currentDate.getHours()).padStart(2, '0');
-            const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-            const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-            const milliseconds = String(currentDate.getMilliseconds()).padStart(3, '0');
-        
-            const currentTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
-            console.log(currentTime);
-
+            // use UTC time to avoid timezone issues
+            const currentTime = new Date().toUTCString();
+            
             if (
                 data.pfp === '' ||
                 !data.pfp.includes('https') ||
