7ee47fba9e33cf8266aea801e3b283c81abd78a3
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-06-25 20:42:18 +0530

    🚧 Routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+// // vite.config.js
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // Other Vite configuration options...
+  plugins : [react()],
+  server: {
+    // Ensure that the server fallback is set to index.html
+    // This allows React Router to handle the routing on the client-side
+    fs: {
+      strict: false,
+    },
+    historyApiFallback: true,
+  },
+});
