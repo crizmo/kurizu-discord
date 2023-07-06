903904a5407c928f5f2a5a6ae7694f042c41ad2e
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-07-06 10:30:32 +0530

    🔥 New category

--- src/data/channels.js
+++ src/data/channels.js
@@ -9,23 +9,32 @@ const channels = [
     },
     {
         category: '⭐ General',
-        channels: [{ id: 4, name: 'General', unreadCount: 0, isActive: false, navigateTo: '/general' }],
+        channels: [
+            { id: 4, name: 'General', unreadCount: 0, isActive: false, navigateTo: '/general' }
+        ],
     },
     {
         category: '📂 Projects',
         channels: [
-            { id: 5, name: 'AnyAnime', unreadCount: 0, isActive: false, navigateTo: '/anyanime' },
-            { id: 6, name: 'Image2Url', unreadCount: 0, isActive: false, navigateTo: '/image2url' },
-            { id: 7, name: 'Discord Cards', unreadCount: 0, isActive: false, navigateTo: '/discordcards' },
-            { id: 8, name: 'Elina', unreadCount: 0, isActive: false, navigateTo: '/elina' },
-            { id: 9, name: 'Cordle', unreadCount: 0, isActive: false, navigateTo: '/cordle' },
-            { id: 10, name: 'Timely', unreadCount: 0, isActive: false, navigateTo: '/timely' },
-            { id: 11, name: 'Emogit', unreadCount: 0, isActive: false, navigateTo: '/emogit' },
-            { id: 12, name: 'Minikey', unreadCount: 0, isActive: false, navigateTo: '/minikey' },
-            { id: 13, name: 'Type3D', unreadCount: 0, isActive: false, navigateTo: '/type3d' },
-            { id: 14, name: 'Pixit', unreadCount: 0, isActive: false, navigateTo: '/pixit' },
+            { id: 5, name: 'Discord Cards', unreadCount: 0, isActive: false, navigateTo: '/discordcards' },
+            { id: 6, name: 'Elina', unreadCount: 0, isActive: false, navigateTo: '/elina' },
+            { id: 7, name: 'Cordle', unreadCount: 0, isActive: false, navigateTo: '/cordle' },
+            { id: 8, name: 'Timely', unreadCount: 0, isActive: false, navigateTo: '/timely' },
+            { id: 9, name: 'Emogit', unreadCount: 0, isActive: false, navigateTo: '/emogit' },
+            { id: 10, name: 'Minikey', unreadCount: 0, isActive: false, navigateTo: '/minikey' },
+            { id: 11, name: 'Type3D', unreadCount: 0, isActive: false, navigateTo: '/type3d' },
+            { id: 12, name: 'Pixit', unreadCount: 0, isActive: false, navigateTo: '/pixit' },
         ],
     },
+    {
+        category: '📦 NPM packages',
+        channels: [
+            { id: 13, name: 'AnyAnime', unreadCount: 0, isActive: false, navigateTo: '/anyanime' },
+            { id: 14, name: 'Image2Url', unreadCount: 0, isActive: false, navigateTo: '/image2url' },
+            { id: 15, name: 'JNG', unreadCount: 0, isActive: false, navigateTo: '/jng' },
+        ],
+    },
+
 ];
 
 export default channels;
