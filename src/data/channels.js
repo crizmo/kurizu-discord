b132739469896288d1e8b5a0cf26c5b5679918d7
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-07-12 23:37:09 +0530

    🔥 Ping

--- src/data/channels.js
+++ src/data/channels.js
@@ -2,39 +2,39 @@ const channels = [
     {
         category: '🔮 Info',
         channels: [
-            { id: 1, name: 'Home', unreadCount: 0, isActive: false, navigateTo: '/' },
-            { id: 2, name: 'Announcements', unreadCount: 1, isActive: false, navigateTo: '/announcements' },
-            { id: 3, name: 'About', unreadCount: 0, isActive: false, navigateTo: '/about' },
+            { id: 1, name: 'Home', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/' },
+            { id: 2, name: 'Announcements', unreadCount: 1, ping: 1, isActive: false, navigateTo: '/announcements' },
+            { id: 3, name: 'About', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/about' },
         ],
     },
     {
         category: '⭐ General',
         channels: [
-            { id: 4, name: 'General', unreadCount: 0, isActive: false, navigateTo: '/general' }
+            { id: 4, name: 'General', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/general' }
         ],
     },
     {
         category: '📂 Projects',
         channels: [
-            { id: 5, name: 'Discord Cards', unreadCount: 0, isActive: false, navigateTo: '/discordcards' },
-            { id: 6, name: 'Elina', unreadCount: 0, isActive: false, navigateTo: '/elina' },
-            { id: 7, name: 'Cordle', unreadCount: 0, isActive: false, navigateTo: '/cordle' },
-            { id: 8, name: 'Timely', unreadCount: 0, isActive: false, navigateTo: '/timely' },
-            { id: 9, name: 'Emogit', unreadCount: 0, isActive: false, navigateTo: '/emogit' },
-            { id: 10, name: 'Minikey', unreadCount: 0, isActive: false, navigateTo: '/minikey' },
-            { id: 11, name: 'Type3D', unreadCount: 0, isActive: false, navigateTo: '/type3d' },
-            { id: 12, name: 'Pixit', unreadCount: 0, isActive: false, navigateTo: '/pixit' },
-            { id: 13, name: 'Repler', unreadCount: 0, isActive: false, navigateTo: '/repler' },
+            { id: 5, name: 'Discord Cards', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/discordcards' },
+            { id: 6, name: 'Elina', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/elina' },
+            { id: 7, name: 'Cordle', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/cordle' },
+            { id: 8, name: 'Timely', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/timely' },
+            { id: 9, name: 'Emogit', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/emogit' },
+            { id: 10, name: 'Minikey', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/minikey' },
+            { id: 11, name: 'Type3D', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/type3d' },
+            { id: 12, name: 'Pixit', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/pixit' },
+            { id: 13, name: 'Repler', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/repler' },
         ],
     },
     {
         category: '📦 NPM packages',
         channels: [
-            { id: 14, name: 'AnyAnime', unreadCount: 0, isActive: false, navigateTo: '/anyanime' },
-            { id: 15, name: 'Image2Url', unreadCount: 0, isActive: false, navigateTo: '/image2url' },
-            { id: 16, name: 'XLSX-Mongo', unreadCount: 0, isActive: false, navigateTo: '/xlsx-mongo' },
-            { id: 17, name: 'JNG', unreadCount: 0, isActive: false, navigateTo: '/jng' },
-            { id: 18, name: 'CustomJS', unreadCount: 0, isActive: false, navigateTo: '/customjs' },
+            { id: 14, name: 'AnyAnime', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/anyanime' },
+            { id: 15, name: 'Image2Url', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/image2url' },
+            { id: 16, name: 'XLSX-Mongo', unreadCount: 1, ping: 0, isActive: false, navigateTo: '/xlsx-mongo' },
+            { id: 17, name: 'JNG', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/jng' },
+            { id: 18, name: 'CustomJS', unreadCount: 0, ping: 0, isActive: false, navigateTo: '/customjs' },
         ],
     },
 
