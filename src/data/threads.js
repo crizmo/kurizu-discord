2af73c0493db848fbf179a74863ea6d8aa9654d5
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-07-12 23:37:22 +0530

    🔥 Threads

---
+++ src/data/threads.js
@@ -0,0 +1,12 @@
+const threads = [
+    {
+        id: 1,
+        name: 'API',
+        unreadCount: 0,
+        isActive: false,
+        parentEndpoint: '/xlsx-mongo',
+        navigateTo: '/xlsx-mongo/api'
+    },
+];
+
+export default threads;
