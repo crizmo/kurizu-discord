63e89d6245eadc56cda4097487783b03d07dffe3
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-11-04 08:54:24 +0530

    🔮 Status <> About

--- src/data/users.js
+++ src/data/users.js
@@ -10,98 +10,112 @@ const users = [
             'User', 
             'Code',
         ],
-        about: 'I like to code :D'
+        about: 'Kuri 🐈 <a href="https://kurizu.vercel.app/" target="_blank">kurizu.vercel.app</a> <br /><br/> Feel free to follow me on GitHub at <a href="https://github.com/crizmo" target="_blank">https://github.com/crizmo</a>',
+        status: 'I like to code :D'
     },
     {
         id: 2,
         username: 'Pocky',
         avatar: 'https://cdn.discordapp.com/avatars/806015103198822401/a7224a4e4806e4fa9d694e2c5ca31e70.webp',
         roles: ['Admin'],
-        about: 'Fart'
+        about: 'Online',
+        status: 'Fart'
     },
     {
         id: 3,
         username: 'Count Trapula',
         avatar: 'https://cdn.discordapp.com/attachments/977949070893125632/1126380203413868604/counttrapula.png',
         roles: ['Admin'],
-        about: 'Clone'
+        about: 'Online',
+        status: 'Clone'
     },
     {
         id: 4,
         username: 'Syeca',
         avatar: 'https://cdn.discordapp.com/avatars/782653471354650684/ace466bef93d7804a041e1ca6a5ac942.webp',
         roles: ['User'],
-        about: 'Code'
+        about: 'Online',
+        status: 'Code'
     },
     {
         id: 5,
         username: 'Raphael',
         avatar: 'https://cdn.discordapp.com/attachments/970974282681307187/1122765302762053694/raph.png',
         roles: ['White'],
-        about: 'Coding >_< is cool !'
+        about: 'Online',
+        status: 'Coding >_< is cool !'
     },
     {
         id: 6,
         username: 'coding398',
         avatar: 'https://cdn.discordapp.com/avatars/626618189450838027/451d6abf0fb1b7850ba08d29cbb60978.webp?size=1024',
         roles: ['Code'],
-        about: "G'day lads"
+        about: 'Online',
+        status: "G'day lads"
     },
     {
         id: 7,
         username: 'chicken.poop',
         avatar: 'https://cdn.discordapp.com/avatars/844689790075011083/baac9006dd249a6e4c01f361fa076c27',
         roles: ['Code'],
-        about: 'Poop'
+        about: 'Online',
+        status: 'Poop'
     },
     {
         id: 8,
         username: 'Swave',
         avatar: 'https://cdn.discordapp.com/attachments/977949070893125632/1126379608086953984/swave.png',
         roles: ['User'],
-        about: 'Person below hates Hu Tao'
+        about: 'Online',
+        status: 'Person below hates Hu Tao'
     },
     {
         id: 9,
         username: 'Hu Tao',
         avatar: 'https://cdn.discordapp.com/attachments/977949070893125632/1126382605865074858/hutao.png',
         roles: ['User'],
-        about: 'Simping for Hu Tao'
+        about: 'Online',
+        status: 'Simping for Hu Tao'
     },
     {
         id: 10,
         username: 'Liam',
         avatar: 'https://cdn.discordapp.com/attachments/437067256049172491/1121773162967867553/5.png',
         roles: ['User'],
-        about: 'Developer'
+        about: 'Online',
+        status: 'Developer'
     },
     {
         id: 11,
         username: 'ErikoXDev',
         avatar: 'https://cdn.discordapp.com/attachments/1122075854923632690/1122076009185947648/eriko4klogoFull.png',
         roles: ['User'],
-        about: 'Baller'
+        about: 'Online',
+        status: 'Baller'
     },
     {
         id: 12,
         username: 'Kat21',
         avatar: 'https://cdn.discordapp.com/attachments/1122159848663027822/1122160050740412467/BigCat01.png',
         roles: ['User'],
-        about: 'Hi there 👋'
+        about: 'Online',
+        status: 'Hi there 👋'
     },
     {
         id: 13,
         username: 'dainfloop',
         avatar: 'https://cdn.discordapp.com/attachments/970974282681307187/1122765027452133437/daln.png',
         roles: ['User'],
-        about: 'chicken nuggets'
+        about: 'Online',
+        status: 'chicken nuggets'
     },
     {
         id: 14,
         username: 'Oli',
         avatar: 'https://cdn.discordapp.com/avatars/259392249693995008/c15ae250f485553b148306cb5adae554.webp',
         roles: ['Admin'],
-        about: 'I was here'
+        about: 'Online',
+        status: 'I was here'
     }
 ];
 
